1432bff6fba730337b07df4891095c10193a6b26
DhruvKothari19 <dhruv.ak@somaiya.edu>
2022-04-09 12:18:59 +0530

    [Flask Temp]

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+	return 'Hello World'
+
+# main driver function
+if __name__ == '__main__':
+	app.run()
